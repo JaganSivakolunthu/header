d6f7be6c896f247336eddf2e5e3d9d9bf6b74031
Jagan S <wittyjag@gmail.com>
2025-11-25 23:42:10 +0530

    Add header file with type definitions and macros

---
+++ header.h
@@ -0,0 +1,14 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+typedef unsigned char u8;
+typedef unsigned int u32;
+
+typedef signed char s8;
+typedef signed int s32;
+
+#define DATA_PINS 0xFF
+#define RS 1<<8
+#define EN 1<<9
+
+#endif
