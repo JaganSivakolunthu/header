829bfd61899c620497b37ce4b468867cc04e547e
Jagan S <wittyjag@gmail.com>
2025-12-04 11:04:40 +0530

    Add CAN message struct to header.h
    Added a new struct 'can_frame' for CAN message representation.

--- header.h
+++ header.h
@@ -6,9 +6,18 @@ typedef unsigned int u32;
 
 typedef signed char s8;
 typedef signed int s32;
+typedef float f32;
 
 #define DATA_PINS 0xFF
 #define RS 1<<8
 #define EN 1<<9
+
+typedef struct can_frame{
+	u32 m_id;
+	u32 rtr;
+	u32 dlc;
+	u32 byteA;
+	u32 byteB;
+} can_msg;
 
 #endif
