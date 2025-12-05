e7b915cc6bd717806daabbe7a490c6ecfde18977
Jagan S <wittyjag@gmail.com>
2025-12-05 09:42:24 +0530

    Clean up header.h by removing unused code
    Removed unused definitions and function prototypes.

--- header.h
+++ header.h
@@ -2,34 +2,13 @@
 #define HEADER_H
 
 typedef unsigned char u8;
+typedef unsigned short int u16;
 typedef unsigned int u32;
 
 typedef signed char s8;
+typedef signed short int s16;
 typedef signed int s32;
+
 typedef float f32;
 
-#define DATA_PINS 0xFF
-#define RS 1<<8
-#define EN 1<<9
-
-typedef struct can_frame{
-	u32 m_id;
-	u32 rtr;
-	u32 dlc;
-	u32 byteA;
-	u32 byteB;
-} can_msg;
-
-void delay_ms(u32 );
-void LCD_INIT(void);
-void LCD_COMMAND(u8 );
-void LCD_DATA(u8 );
-void LCD_STRING(const u8 *);
-void LCD_INTEGER(s32 );
-void LCD_FLOAT(f32 );
-void LCD_BINARY(u32 );
-
 #endif
-
-
-
